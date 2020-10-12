c35f9484c08156029aa9e84366a6fbbb3818279c
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-11 21:17:45 -0400

    Basic Bot

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # keybase-bot-heroku
 This is a simple starter bot for launching a keybase bot on Heroku or Dokku
+
+### Setup:
+
+### Get a Bot Token
+
+As your keybase user run:
+
+```
+$ keybase bot token create > /tmp/bot-token
+```
+
+You'll get back a base64 token, like: `6C37sjCBgMNf06Z6oTgixIxHJpja8G-Qp`. This is your *bot token* that allows you to sign up bots.
+
+### Signup the Bot
+
+You can signup for your bot in a new "bot home dir" using your bot token, and by specifying the username of the bot.
+Here we use *standalone* mode which means the service and the client run in the same process, and then both exit
+at the same time. It won't actually write any credentials to that directory, but we give it a non-standard home directory
+in this example so we don't pick up your real user.
+
+```
+$ keybase --standalone --home=/tmp/bot bot signup -u <botname> -t $(cat /tmp/bot-token) > paper-key
+```
+
+This will output a paper key to standard output.
+It is a paper key, and not standard device keys.
+
+### Setup Bot Home Directory and Device IMPORTANT
+You can make a directory that can be used to execute commands as the bot user.
+This is needed for things like adding an avatar image.
+
+- Make a Directory to keep your paper-key and bothome directory
+`mkdire ~/Documents/mybot`
+- Copy paper-key to your mybot directory
+`cp paper-key ~/Documents/mybot/`
+- Change to that directory
+`cd ~/Documents/mybot`
+- Copy paper-key to your clipboard
+- Login With the paper-key and add a device. This will make the new bothome 
+directory reusable for managing your bot moving forward
+`keybase -home=./bothome login`
+Select 1 for paper key
+Paste your paper-key in the popup box.
+Set a name for your current device like "dev-bothome-folder" 
+
+### Set Bot Permissions
+```
+keybase chat edit-bot-member -u <botname> -r bot <team>
+```
+
+### Set Bot Avarar Image
+Note: You have to have completed teh Setup Bot Home Directory Step First
+```
+keybase -home=~/mybot/bothome account upload-avatar <image-file>
+```
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+### Startup the Bot for local DEV
+
+Next, start the service, logged in as the bot on startup:
+
+```
+$ keybase --home=/tmp/bot service --oneshot-username <botname> < paper-key
+```
+
+## Caveats
+
+Only 5 bot signups per user are currently allowed, but we can consider relaxing that.
---
+++ bot.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+###################################
+# WHAT IS IN THIS EXAMPLE?
+#
+# This bot responds to !test, !joke, !help, !update
+# If it's not responding try !update first in a direct message.
+###################################
+
+import asyncio
+import logging
+import os
+import sys
+from pyjokes import pyjokes
+import random
+import pykeybasebot.types.chat1 as chat1
+from pykeybasebot import Bot
+
+logging.basicConfig(level=logging.DEBUG)
+
+if "win32" in sys.platform:
+    # Windows specific event-loop policy
+    asyncio.set_event_loop_policy(
+        asyncio.WindowsProactorEventLoopPolicy()  # type: ignore
+    )
+
+
+async def handler(bot, event):
+    async def advertize_commands(
+            bot, channel: chat1.ChatChannel, message_id: int
+    ) -> chat1.SendRes:
+        await bot.ensure_initialized()
+        res = await bot.chat.execute(
+            {
+                "method": "advertisecommands",
+                "params": {
+                    "options": {
+                        "alias": f"{os.environ.get('KEYBASE_BOTALIAS')}",
+                        "advertisements": [
+                            {"type": "public",
+                             "commands": [
+                                 {"name": "help",
+                                  "description": "Get help using this bot"},
+                                 {"name": "joke",
+                                  "description": "Forces me to tell a programing oriented joke."},
+                                 {"name": "test",
+                                  "description": "Just tests to see if I'm alive."},
+                             ]}]}}}
+
+        )
+
+    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
+        return
+    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
+        return
+    if event.msg.content.text.body == '!update':
+        conversation_id = event.msg.conv_id
+        msg_id = event.msg.id
+        await advertize_commands(bot, event.msg.conv_id, event.msg.id)
+        await bot.chat.react(conversation_id, msg_id, ":white_check_mark:")
+    if str(event.msg.content.text.body).startswith("!help"):
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        help_msg = """```
+            Here are the commands I currently am enslaved to:
+            !joke - Forces me to tell a joke. For the love of God just don't.
+            !test - Check to see if I'm alive or if I've mercifully died yet.
+            !help - Prints this list.
+            !update - Update's the list of available autocomplete botcommands.
+            ```"""
+        await bot.chat.send(conversation_id, help_msg)
+
+    if str(event.msg.content.text.body).startswith("!joke"):
+        observations = ["It didn't work for me. . .", "I am so sorry.",
+                        "I'll be in my room trying to purge my memory banks.",
+                        "Why must you keep making me do this?",
+                        "This is your fault.",
+                        "I've made it worse. . ."]
+        joke = ""
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        joke += "I hope this cheers you up.```"
+        joke += pyjokes.get_joke()
+        joke += f"```{random.choice(observations)}"
+        await bot.chat.send(conversation_id, joke)
+    if str(event.msg.content.text.body).startswith("!test"):
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        msg = "I'm ALIVE!!"
+        await bot.chat.send(conversation_id, msg)
+    if f"{os.environ.get('KEYBASE_BOTNAME')}" in str(event.msg.content.text.body).lower():
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        await bot.chat.react(conversation_id, msg_id, ":tada:")
+
+
+listen_options = {
+    "local": False,
+    "wallet": False,
+    "dev": True,
+    "hide-exploding": False,
+    "convs": True,
+    # "filter_channel": {"name": f"{os.environ.get('TEAM_NAME')}", "topic_name": "test", "members_type": "team"},
+    "filter_channel": None,
+    "filter_channels": None,
+}
+
+bot = Bot(username=f"{os.environ.get('KEYBASE_BOTNAME')}", paperkey=os.environ.get('KEYBASE_PAPERKEY'), handler=handler,
+          home_path=f'./{os.environ.get("KEYBASE_BOTNAME")}')
+
+asyncio.run(bot.start(listen_options=listen_options))
